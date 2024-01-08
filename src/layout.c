10958ac2b1755c25dba4b423e8f1fc8ae691688b
gauravmanmode <gauravmanmode@gmail.com>
2024-01-08 23:41:02 +0530

    Update layout.c

--- src/layout.c
+++ src/layout.c
@@ -3,18 +3,18 @@
 #include "matrix.h"
 #include "report.h"
 
-#define LAYOUT_60( \
-                   K00_0, K01_0, K02_0, K03_0, K04_0, K05_0, K06_0, K07_0, K08_0, K09_0, K10_0, K11_0, K12_0, K13_0, \
-                   K00_1, K01_1, K02_1, K03_1, K04_1, K05_1, K06_1, K07_1, K08_1, K09_1, K10_1, K11_1, K12_1, K13_1, \
-                   K00_2, K01_2, K02_2, K03_2, K04_2, K05_2, K06_2, K07_2, K08_2, K09_2, K10_2, K11_2,        K13_2, \
+#define LAYOUT_70( \
+                   K00_0, K01_0, K02_0, K03_0, K04_0, K05_0, K06_0, K07_0, K08_0, K09_0, K10_0, K11_0, K12_0, K13_0, K14_0, K15_0, \
+                   K00_1, K01_1, K02_1, K03_1, K04_1, K05_1, K06_1, K07_1, K08_1, K09_1, K10_1, K11_1, K12_1, K13_1, K14_1, K15_1, \
+                   K00_2, K01_2, K02_2, K03_2, K04_2, K05_2, K06_2, K07_2, K08_2, K09_2, K10_2, K11_2,        K13_2, K14_2, K15_2 \
                    K00_3, K01_3, K02_3, K03_3, K04_3, K05_3, K06_3, K07_3, K08_3, K09_3, K10_3, K13_3, K14_3, K15_3, \
-                   K00_4, K01_4, K02_4,                      K05_4,               K08_4, K09_4, K10_4, K13_4, K14_4 \
+                   K00_4, K01_4, K02_4,                      K05_4,               K08_4, K09_4, K10_4, K13_4, K15_4, K14_4 \
                  ) { \
-    { K00_0, K01_0, K02_0, K03_0, K04_0, K05_0, K06_0, K07_0, K08_0, K09_0, K10_0, K11_0, K12_0, K13_0, KC_NO, KC_NO }, \
-    { K00_1, K01_1, K02_1, K03_1, K04_1, K05_1, K06_1, K07_1, K08_1, K09_1, K10_1, K11_1, K12_1, K13_1, KC_NO, KC_NO }, \
-    { K00_2, K01_2, K02_2, K03_2, K04_2, K05_2, K06_2, K07_2, K08_2, K09_2, K10_2, K11_2, KC_NO, K13_2, KC_NO, KC_NO }, \
+    { K00_0, K01_0, K02_0, K03_0, K04_0, K05_0, K06_0, K07_0, K08_0, K09_0, K10_0, K11_0, K12_0, K13_0, K14_0, K15_0 }, \
+    { K00_1, K01_1, K02_1, K03_1, K04_1, K05_1, K06_1, K07_1, K08_1, K09_1, K10_1, K11_1, K12_1, K13_1, K14_1, K14_1 }, \
+    { K00_2, K01_2, K02_2, K03_2, K04_2, K05_2, K06_2, K07_2, K08_2, K09_2, K10_2, K11_2, KC_NO, K13_2, K14_2, K14_2 }, \
     { K00_3, K01_3, K02_3, K03_3, K04_3, K05_3, K06_3, K07_3, K08_3, K09_3, K10_3, KC_NO, KC_NO, K13_3, K14_3, K15_3 }, \
-    { K00_4, K01_4, K02_4, KC_NO, KC_NO, K05_4, KC_NO, KC_NO, K08_4, K09_4, K10_4, KC_NO, KC_NO, K13_4, K14_4, KC_NO } \
+    { K00_4, K01_4, K02_4, KC_NO, KC_NO, K05_4, KC_NO, KC_NO, K08_4, K09_4, K10_4, KC_NO, KC_NO, K13_4, K14_4, K15_4 } \
 }
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
@@ -30,24 +30,24 @@ enum custom_keycodes {
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Keymap _BL: (Base Layer) Default Layer
-     * ,------------------------------------------------------------.
-     * |Fn |  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|     Gui|
-     * |------------------------------------------------------------|
-     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Backsp|
-     * |------------------------------------------------------------|
-     * |Gui   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|  Return|
-     * |------------------------------------------------------------|
-     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shi| NO| NO|
-     * |------------------------------------------------------------|
-     * |App |Alt |Ctl |          Space          |Ctl|Alt |App|Fn| NO|
-     * `------------------------------------------------------------'
+     * ,------------------------------------------------------------------------.
+     * |Fn |  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|     Gui| Ins | Home
+     * |------------------------------------------------------------------------|
+     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Backsp| Del | End
+     * |------------------------------------------------------------------------|
+     * |Gui   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|  Return| Paus | PgUp
+     * |------------------------------------------------------------------------|
+     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shi| NO| NO| Up | PgDn
+     * |------------------------------------------------------------------------|
+     * |App |Alt |Ctl |          Space          |Ctl|Alt |App|Fn| NO| Left
+     * `------------------------------------------------------------------------'
      */
-    [_BL] = LAYOUT_60(
-        MO(_FL), KC_1,    KC_2,    KC_3,   KC_4,    KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_RGUI,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-        KC_LGUI, KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_NO,   KC_NO,
-        KC_APP,  KC_LALT, KC_LCTL,                           KC_SPC,                     KC_RCTL, KC_RALT, KC_APP,  MO(_FL), KC_NO
+    [_BL] = LAYOUT_70(
+        MO(_FL), KC_1,    KC_2,    KC_3,   KC_4,    KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_INS,   KC_HOME,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,   KC_END,
+        KC_LGUI, KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT, KC_ESC,   KC_PGUP,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
+        KC_LCTL,  KC_APP, KC_LALT,                           KC_SPC,                     KC_RALT, MO(_FL), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     /* Keymap _FL: (Base Layer) Function Layer
@@ -63,12 +63,12 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |    |    |    |                         |   |   |   |   |   |
      * `------------------------------------------------------------'
      */
-    [_FL] = LAYOUT_60(
-        _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_ESC,
-        KC_BSLS, KC_GRV, _______, _______, _______, _______, _______,  KC_HOME, KC_UP,   KC_END,  _______, KC_PGUP, _______, _______,
-        KC_CAPS, KC_F13,  KC_F14,  KC_F15,  KC_F16,  _______, KC_PGUP, KC_LEFT, KC_DOWN, KC_RIGHT,KC_DOWN, KC_UP,            _______,
+    [_FL] = LAYOUT_70(
+        _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_ESC, _______, _______,
+        KC_BSLS, KC_GRV, _______, _______, _______, _______, _______,  KC_HOME, KC_UP,   KC_END,  _______, KC_PGUP, _______, _______, _______, _______,
+        KC_CAPS, KC_F13,  KC_F14,  KC_F15,  KC_F16,  _______, KC_PGUP, KC_LEFT, KC_DOWN, KC_RIGHT,KC_DOWN, KC_UP,            _______, _______, _______,
         _______, KC_F17,  KC_F18,  KC_F19,  KC_F20,  _______, KC_PGDN, _______, KC_DEL,  _______, KC_PGDN, _______, _______, _______,
-        _______, _______, _______,                            _______,                   _______, _______, _______, _______, _______
+        _______, _______, _______,                            _______,                   _______, _______, _______, _______, _______, _______
     ),
 };
 
